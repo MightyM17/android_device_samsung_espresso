3b5932c80170a300bafeb82f95a9d6fdb2270bb9
Mighty <mightymb17@gmail.com>
2021-10-21 17:11:01 +0530

    device-common: DRM

--- device-common.mk
+++ device-common.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dock_kbd_attach
 
+# DRM
+PRODUCT_PACKAGES += \
+    libwvm \
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
